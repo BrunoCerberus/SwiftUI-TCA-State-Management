859151eec7b0b27b753317716e914202b0666b1d
Bruno Lopes <bruno.guitarpro@gmail.com>
2021-10-11 00:50:39 -0300

    added a explicit publisher definition without @Published

--- swiftui-tca-state-management/AppState.swift
+++ swiftui-tca-state-management/AppState.swift
@@ -8,6 +8,22 @@
 import Combine
 
 //@ObservedObject - must perform ObservableObject protocol and updates any View that is subscribed to any of its value and hold any information across the Views
+
+//Simplest definition
+//final class AppState: ObservableObject {
+//    @Published var count: Int = 0
+//}
+
+//Complete definition
 final class AppState: ObservableObject {
-    @Published var count: Int = 0
+    let objectWillChange = ObservableObjectPublisher()
+    
+    //note that @Published property wrapper wrapp all this implementation for you
+    var count = 0 {
+        willSet { self.objectWillChange.send() }
+    }
+    
+    var favoritePrimes: [Int] = [] {
+        willSet { self.objectWillChange.send() }
+    }
 }
