a7524446325ae28fae720678660195f3fb70e9b3
Bruno Lopes <bruno.guitarpro@gmail.com>
2021-10-10 23:20:28 -0300

    first commit

---
+++ swiftui-tca-state-management/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-tca-state-management
+//
+//  Created by bruno on 10/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-tca-state-management/swiftui_tca_state_managementApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_tca_state_managementApp.swift
+//  swiftui-tca-state-management
+//
+//  Created by bruno on 10/10/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_tca_state_managementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
